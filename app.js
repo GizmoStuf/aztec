49d733f742746383f8b29ec2fa44770771e11e2c
GizmoStuf <47191339+GizmoStuf@users.noreply.github.com>
2019-03-07 22:18:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -21,5 +21,16 @@ bot.onText(/\/sunny (.+)/, (msg, match) => {
 bot.onText(/\/start/, (msg, match) => {
   bot.sendMessage(msg.chat.id, 'Chal Bhaag Bosdike')
 });
+// Matches "/echo [whatever]"
+bot.onText(/\/sunny (.+)/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
 
+  const chatId = msg.chat.id;
+  const resp = match[1]; // the captured "whatever"
+
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(-1001396985779, resp);
+});
 
